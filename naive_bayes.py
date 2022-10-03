1e6a871c95b6cb75c014dbdd3998ccc09618e353
Parhamglst <parhamglst@gmail.com>
2022-10-03 04:36:39 -0600

    Final (85.87% accuracy)

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,6 +1,9 @@
 #%% VS Code Notebook
 # Copyright 2022 CMPUT 466 Staff
 # Implement Naive Bayes Classifier with Laplace Smoothing
+from cmath import exp
+import enum
+from turtle import shape
 import numpy as np
 import pandas as pd
 
@@ -23,7 +26,21 @@ class NaiveBayesClassifier:
         self.log_priors = np.zeros(self.num_classes) # Stores P(c)
         self.log_likelihoods = np.zeros((self.num_classes,self.num_features,np.max(X)+1)) # Stores P(e_n|c)
         # !!!!!!!!!!!! Start of your code here !!!!!!!!!!!!
-        pass
+        log_priors_shape = np.shape(self.log_priors)
+        for i in range(self.num_classes):
+            self.log_priors[i] = np.log(np.count_nonzero(y == i)) - np.log(len(y))
+        assert(np.shape(self.log_priors) == log_priors_shape)
+        
+        log_likelihoods_shape = np.shape(self.log_likelihoods)
+        for i in range(len(X)):
+            for j in range(len(X[i])):
+                self.log_likelihoods[y[i]][j][X[i][j]] += 1
+        
+        for i in range(self.num_classes):
+            self.log_likelihoods[i] = np.log(self.log_likelihoods[i] + 1)
+            for j in range(self.num_features):
+                self.log_likelihoods[i][j] -= np.log(np.count_nonzero(y == i) + np.count_nonzero(self.log_likelihoods[i][j] > 1))
+        assert(np.shape(self.log_likelihoods) == log_likelihoods_shape)
         # !!!!!!!!!!!! End of your code here   !!!!!!!!!!!!
 
     def predict_classes(self, X: np.array) -> np.array:
@@ -33,7 +50,13 @@ class NaiveBayesClassifier:
         """
         logs_probs = np.zeros((len(X),self.num_classes))
         # !!!!!!!!!!!! Start of your code here !!!!!!!!!!!!
-        pass
+        predictions = np.zeros(len(X))
+        for x in range(len(X)):
+            for y in range(self.num_classes):
+                for e in range(len(X[x])):
+                    logs_probs[x][y] += self.log_likelihoods[y][e][X[x][e]] + self.log_priors[y]
+            predictions[x] = logs_probs[x].argmax(axis=0)
+        return predictions
         # !!!!!!!!!!!! End of your code here   !!!!!!!!!!!!
 
 # %%
